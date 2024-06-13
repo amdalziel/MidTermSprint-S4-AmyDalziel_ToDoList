f6d13fde5ad046324cba4bf439d4a8c56b3cd4d0
Amy Dalziel <amy.dalziel@keyin.com>
2024-06-13 17:05:59 -0230

    Initial commit

---
+++ src/com/keyin/Main.java
@@ -0,0 +1,8 @@
+package com.keyin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/com/keyin/User.java
@@ -0,0 +1,11 @@
+package com.keyin;
+
+import java.util.LinkedList;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
